ed48fb035ee1b8f6f4fcbaceaaeff651eede9f70
Daniel Gaspar Gonçalves <daniel.gaspar.goncalves@gmail.com>
2022-06-19 13:52:51 -0300

    create line charts

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,18 @@ var status = {
   "T3": {"B":0, "E":0}
 };
 var yield = {};
-var yield_T1 = {
-  "date": [],
-  "yield": [],
-  "color": []
-};
+var yield_T1 = {"date": [], "yield": [], "color": []};
+var yield_T2 = {"date": [], "yield": [], "color": []};
+var yield_T3 = {"date": [], "yield": [], "color": []};
 
 router.get('/', (req, res) => {
   //res.send('It works!');
   res.render('graphs', {
     title: 'Projeto Integrador',
     status,
-    yield,
     yield_T1,
+    yield_T2,
+    yield_T3,
     nome_arquivo: nome_arquivo,
     json
   });
@@ -130,23 +129,39 @@ https.get(url, (resp) => {
       } else {
         yield_T1["date"].push(date);
         yield_T1["yield"].push(yield[date]["T1"]["B"] / (yield[date]["T1"]["B"] + yield[date]["T1"]["E"]));
-        yield_T1["color"].push("rgba(85,85,85,1)");
+        if (yield_T1["yield"][yield_T1["yield"].length-1] < 0.85) {
+          yield_T1["color"].push("rgba(255,0,0,1)");
+        } else {
+          yield_T1["color"].push("rgba(0,255,0,1)");
+        }
       }
-      
+
       if ((yield[date]["T2"]["B"] + yield[date]["T2"]["E"]) == 0) {
-        yield[date]["T2"]["value"] = 0;
-        yield[date]["T2"]["color"] = "rgba(85,85,85,1)";
+        yield_T2["date"].push(date);
+        yield_T2["yield"].push(0);
+        yield_T2["color"].push("rgba(85,85,85,1)");
       } else {
-        yield[date]["T2"]["value"] = yield[date]["T2"]["B"] / (yield[date]["T2"]["B"] + yield[date]["T2"]["E"]);
-        yield[date]["T2"]["color"] = "rgba(255,0,0,1)";
+        yield_T2["date"].push(date);
+        yield_T2["yield"].push(yield[date]["T2"]["B"] / (yield[date]["T2"]["B"] + yield[date]["T2"]["E"]));
+        if (yield_T2["yield"][yield_T2["yield"].length-1] < 0.85) {
+          yield_T2["color"].push("rgba(255,0,0,1)");
+        } else {
+          yield_T2["color"].push("rgba(0,255,0,1)");
+        }
       }
-      
+            
       if ((yield[date]["T3"]["B"] + yield[date]["T3"]["E"]) == 0) {
-        yield[date]["T3"]["value"] = 0;
-        yield[date]["T3"]["color"] = "rgba(85,85,85,1)";
+        yield_T3["date"].push(date);
+        yield_T3["yield"].push(0);
+        yield_T3["color"].push("rgba(85,85,85,1)");
       } else {
-        yield[date]["T3"]["value"] = yield[date]["T3"]["B"] / (yield[date]["T3"]["B"] + yield[date]["T3"]["E"]);
-        yield[date]["T3"]["color"] = "rgba(255,0,0,1)";
+        yield_T3["date"].push(date);
+        yield_T3["yield"].push(yield[date]["T3"]["B"] / (yield[date]["T3"]["B"] + yield[date]["T3"]["E"]));
+        if (yield_T3["yield"][yield_T3["yield"].length-1] < 0.85) {
+          yield_T3["color"].push("rgba(255,0,0,1)");
+        } else {
+          yield_T3["color"].push("rgba(0,255,0,1)");
+        }
       }
     }
     console.log(yield_T1["date"] + ' ' + yield_T1["yield"] + ' ' + yield_T1["color"]);
